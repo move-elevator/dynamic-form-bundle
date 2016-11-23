dd8c7bb5aa8830e39d1d1aece090877e11effe80
Frank Jogeleit <fj@move-elevator.de>
2016-11-23 13:22:44 +0100

    fix test and improvements

---
+++ Tests/Unit/Services/FormField/OptionFilterTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: frankjogeleit
+ * Date: 23.11.16
+ * Time: 12:02
+ */
+
+namespace DynamicFormBundle\Tests\Unit\Services\FormField;
+
+
+class OptionFilterTest
+{
+
+}
