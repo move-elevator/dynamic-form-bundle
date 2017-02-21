f022a296fc6256d1cbc333b41cfa07a23aa6ceb6
Frank Jogeleit <fj@move-elevator.de>
2017-02-21 08:08:33 +0100

    add tests

---
+++ Tests/Functional/Controller/Sonata/FormElementController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace DynamicFormBundle\Tests\Functional\Controller;
+
+use DynamicFormBundle\Tests\Utility\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @package DynamicFormBundle\Tests\Functional\Controller
+ */
+class FormElementControllerTest extends WebTestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->createDatabase();
+
+        $this->loadAliceFixtures([
+            $this->getAliceFixturePath('Test/dynamic_form.yml')
+        ]);
+    }
+
+    public function testAccessCreateAction()
+    {
+        $this->client->request(Request::METHOD_GET, '/admin/dynamic-form/form/1/form-element/headline/create');
+
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testAccessEditAction()
+    {
+        $this->client->request(Request::METHOD_GET, '/admin/dynamic-form/form/1/form-element/1/edit');
+
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testAccessDeleteAction()
+    {
+        $this->client->request(Request::METHOD_GET, '/admin/dynamic-form/form/1/form-element/1/delete');
+
+        $this->assertEquals(Response::HTTP_FOUND, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testAccessCloneAction()
+    {
+        $this->client->request(Request::METHOD_GET, '/admin/dynamic-form/form/1/form-element/1/clone');
+
+        $this->assertEquals(Response::HTTP_FOUND, $this->client->getResponse()->getStatusCode());
+    }
+}
